901815e77e9bab714218b52f40d85904455ef4cf
Mohamed Nour Chrif <MohamedNour.chrif@esprit.tn>
2026-03-24 17:57:32 +0100

    feat: remove SpeedInsights component from main application

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './i18n';
 import './index.css';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { PerformanceModeProvider } from './context/PerformanceModeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PerformanceModeProvider>
       <BrowserRouter>
-        <SpeedInsights />
         <App />
       </BrowserRouter>
     </PerformanceModeProvider>
